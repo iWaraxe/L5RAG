647e9def5190f62e1af6f4eefa638b5db8edea4b
iWaraxe <igor.waraxe@gmail.com>
2025-04-23 19:35:57 +0200

    rag introduction and project configuration

--- src/main/java/com/coherentsolutions/springai/l5rag/L5RagApplication.java
+++ src/main/java/com/coherentsolutions/springai/l5rag/L5RagApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class L5RagApplication {
     public static void main(String[] args) {
         SpringApplication.run(L5RagApplication.class, args);
