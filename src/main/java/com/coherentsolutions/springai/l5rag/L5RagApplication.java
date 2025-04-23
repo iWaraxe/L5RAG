b3075ef7d25b00cf19d52ec96005d2672f8ba832
iWaraxe <igor.waraxe@gmail.com>
2025-04-23 17:33:40 +0200

    Initial Commit

--- src/main/java/com/coherentsolutions/springai/l5rag/L5RagApplication.java
+++ src/main/java/com/coherentsolutions/springai/l5rag/L5RagApplication.java
@@ -2,12 +2,11 @@ package com.coherentsolutions.springai.l5rag;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class L5RagApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(L5RagApplication.class, args);
     }
-
 }
